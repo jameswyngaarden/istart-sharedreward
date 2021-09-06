03ebceb86f9895b0c584dd698cb6b3ad26904ded
DVSneuro <david.v.smith@temple.edu>
2021-09-06 10:18:49 -0400

    retry sub-1300

--- code/L2stats.sh
+++ code/L2stats.sh
@@ -20,7 +20,7 @@ if [ $sub -eq 1240 ] || [ $sub -eq 1245 ] || [ $sub -eq 1247 ] || [ $sub -eq 100
 fi
 
 # hopefully temporary:
-if [ $sub -eq 1002 ] || [ $sub -eq 1010 ] || [ $sub -eq 1253 ] || [ $sub -eq 1286 ] || [ $sub -eq 1300 ]; then # bad data
+if [ $sub -eq 1002 ] || [ $sub -eq 1010 ] || [ $sub -eq 1253 ] || [ $sub -eq 1286 ]; then # bad data
 	echo "skipping sub-${sub} for task-${task}"
 	exit
 fi
