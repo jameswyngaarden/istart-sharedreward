02c2b0e4efe36483280705e6f3175f0bf9476631
DVSneuro <david.v.smith@temple.edu>
2021-09-05 23:11:58 -0400

    logging

--- code/L1stats.sh
+++ code/L1stats.sh
@@ -140,7 +140,7 @@ else # otherwise, do activation and seed-based ppi
 	if [ -e ${OUTPUT}.feat/cluster_mask_zstat1.nii.gz ]; then
 		exit
 	else
-		echo "missing: $OUTPUT " >> ${maindir}/re-runL1.log
+		echo "missing feat output: $OUTPUT " >> ${maindir}/re-runL1.log
 		rm -rf ${OUTPUT}.feat
 	fi
 
