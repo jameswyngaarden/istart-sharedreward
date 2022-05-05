b44e14f749dd37e90a6e753d13e0dd8e1dab1901
orizaff <orizaff@udel.edu>
2022-05-05 10:48:13 -0400

    first model 3 act w covar template, featwatcher off

--- code/L3stats.sh
+++ code/L3stats.sh
@@ -19,14 +19,14 @@ maindir="$(dirname "$scriptdir")"
 
 # study-specific inputs and general output folder
 task=sharedreward
-N=47
+N=43
 copenum=$1
 copenum_thresh_randomise=7 # actual contrasts start here. no need to do randomise main effects (e.g., reward > nothing/fixation/baseline)
 copename=$2
 REPLACEME=$3 # this defines the parts of the path that differ across analyses
 logfile=$4
 
-MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-2_task-${task}_n${N}_flame1_voxel-analysis
+MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-3_task-${task}_n${N}_flame1_testing
 mkdir -p $MAINOUTPUT
 
 
