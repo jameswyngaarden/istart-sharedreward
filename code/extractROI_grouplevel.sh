22c466f12dd0d85a34bfaa7ad00fd325a554ba39
orizaff <orizaff@udel.edu>
2022-06-09 14:44:58 -0400

    extractROI for VS

--- code/extractROI_grouplevel.sh
+++ code/extractROI_grouplevel.sh
@@ -5,16 +5,16 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
 # ROI name and other path information
-for ROI in NAcc; do
+for ROI in VS_thr5; do
 	MASK=${maindir}/masks/seed-${ROI}.nii.gz
 	TASK=sharedreward
 	TYPE=act
 	outputdir=${maindir}/derivatives/imaging_plots
 	mkdir -p $outputdir
 
-	for COPENUM in 7; do
+	for COPENUM in 7 8 9 10 11 13 14 16; do
 		cnum_padded=`zeropad ${COPENUM} 2`
-		MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-2_task-${TASK}_n47_flame1
+		MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-3_task-${TASK}_n45_flame1_SFN_edition
 		DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${cnum_padded}_*onegroup.gfeat/cope1.feat/filtered_func_data.nii.gz`
 		fslmeants -i $DATA -o ${outputdir}/${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
 
