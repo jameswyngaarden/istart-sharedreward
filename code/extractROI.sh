f08410971b36a7089be0ad56b8b927e41c25d789
orizaff <orizaff@udel.edu>
2022-08-19 19:34:12 -0400

    ppi ROIs extracted as txt for copes 1 to 6

--- code/extractROI.sh
+++ code/extractROI.sh
@@ -3,17 +3,22 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
+outputdir=${maindir}/derivatives/imaging_plots
+TASK=sharedreward
+MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-2_task-${TASK}_n45_flame1_SFN_edition
+mkdir -p $outputdir
 
-for sub in `cat ${maindir}/code/newsubs.txt`; do	
+
 	# ROI name and other path information
-	for ROI in ppi_C10_FS-C_z8_sub-neg_cluster1 ppi_C10_FS-C_z8_sub-neg_cluster2 ppi_C13_rew-pun_F-C_z12_su-rs2_cluster1 ppi_C13_rew-pun_F-C_z1_main-effect ppi_C16_rew_F-C_z1_main-effect_cluster1 ppi_C16_rew_F-C_z1_main-effect_cluster2 ppi_C21_rew_F-SC_z1_main-effect ppi_C23_rew-pun_F-SC_z12_sub-neg_cluster1 ppi_c9_F-C_sub-neg; do
-		for TASK in sharedreward; do
-			for run in 1 2; do
-				MASK=${maindir}/masks/${ROI}.nii.gz
-				#MASK=${maindir}/masks/seed-${ROI}.nii.gz
-				fslmeants -i /data/projects/istart-data/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_run-${run}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz -m ${MASK} -o sub-${sub}_task-${TASK}_run-${run}_${ROI}.txt
-				mv sub-${sub}_task-${TASK}_run-${run}_${ROI}.txt ${maindir}/derivatives/fsl/sub-${sub}/
-			done		
-		done
+for ROI in ppi_C10_FS-C_z8_sub-neg_cluster1 ppi_C10_FS-C_z8_sub-neg_cluster2 ppi_C13_rew-pun_F-C_z12_su-rs2_cluster1 ppi_C13_rew-pun_F-C_z1_main-effect ppi_C16_rew_F-C_z1_main-effect_cluster1 ppi_C16_rew_F-C_z1_main-effect_cluster2 ppi_C21_rew_F-SC_z1_main-effect ppi_C23_rew-pun_F-SC_z12_sub-neg_cluster1 ppi_c9_F-C_sub-neg; do
+	for TASK in sharedreward; do
+		MASK=${maindir}/masks/${ROI}.nii.gz
+		#MASK=${maindir}/masks/seed-${ROI}.nii.gz
+		TYPE=ppi_seed-VS_thr5
+		for COPENUM in 1 2 3 4 5 6; do # act
+			cnum_padded=`zeropad ${COPENUM} 2`
+			DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${cnum_padded}_*.gfeat/cope1.feat/filtered_func_data.nii.gz`
+			fslmeants -i $DATA -o ${outputdir}/${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
+		done	
 	done
 done
