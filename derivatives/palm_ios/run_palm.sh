83692eb4569aa6502b657e99323d7d7b70d08251
orizaff <orizaff@udel.edu>
2023-02-10 19:12:41 -0500

    remade STG mask and extracted betas, also added new matlab barplots to figures

--- derivatives/palm_ios/run_palm.sh
+++ derivatives/palm_ios/run_palm.sh
@@ -1,1 +1,1 @@
-/data/tools/palm-alpha119/palm -i df_palm.csv -d ios_model.mat -t ios_model.con -o palm_ios_output/palm_ios_model-1 -corrcon
+/data/tools/palm-alpha119/palm -i df_palm_ppi.csv -d ios_model.mat -t ios_model.con -o palm_ios_output/palm_ios_model-1 -corrcon
