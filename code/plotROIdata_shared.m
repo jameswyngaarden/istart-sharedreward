01ea2e5e443dad0c11421b85897f5645b40b426b
orizaff <orizaff@udel.edu>
2023-04-28 14:28:31 -0400

    FFA barplot

--- code/plotROIdata_shared.m
+++ code/plotROIdata_shared.m
@@ -19,8 +19,8 @@ maindir = '/data/projects/istart-sharedreward';
 roidir = '/data/projects/istart-sharedreward/derivatives/imaging_plots/'; % Results from extractROI script.
 resultsdir = '/data/projects/istart-sharedreward/derivatives/imaging_plots/results/'; % Output where results will be saved.
 
-rois = {'act_C14_rew_F-S_z2_sub'}; %{'resampled_pTPJ-thr50-2mm'}; %{'act_C14_rew_F-S_z1_main_cluster2'}; %{'seed-VS_thr5'}; %{'resampled_aTPJ-thr50-2mm' 'resampled_pTPJ-thr50-2mm'}; % 'target-pcc_bin' 'target-FFA_bin' 'target-vmPFC_bin'}; %'seed-VS_thr5' 'seed-vmPFC-5mm-thr' 'seed-mPFC-thr' %'ppi_C10_FS-C_z8_sub-neg_cluster1' 'ppi_C23_rew-pun_F-SC_z12_sub-neg_cluster3'} 
-models = {'_type-act_'}; %{'_type-ppi_seed-VS_thr5_'};
+rois =  {'ppi_C16_rew_F-C_z1_main-effect_cluster1'}; %{'seed-VS_thr5'}; %{'act_C14_rew_F-S_z2_sub'} %{'resampled_pTPJ-thr50-2mm'}; %{'act_C14_rew_F-S_z1_main_cluster2'}; %{'resampled_aTPJ-thr50-2mm' 'resampled_pTPJ-thr50-2mm'}; % 'target-pcc_bin' 'target-vmPFC_bin'}; %'seed-VS_thr5' 'seed-vmPFC-5mm-thr' 'seed-mPFC-thr' %'ppi_C10_FS-C_z8_sub-neg_cluster1' 'ppi_C23_rew-pun_F-SC_z12_sub-neg_cluster3'} 
+models = {'_type-ppi_seed-VS_thr5_'};%{'_type-act_'};%
 
 % Test hypotheses:
 
@@ -30,7 +30,7 @@ H3 = 1;
 
 if H3 == 1
    
-        name = 'Act_Result'; %'PPI_Result';
+        name = 'PPI_Result';%'Act_Result'; %
         R_Friend = {'cope-04.txt'};
         R_Stranger = {'cope-06.txt'};
         R_Computer = {'cope-02.txt'};
@@ -39,6 +39,6 @@ if H3 == 1
         P_Computer = {'cope-01.txt'};
         
         
-        type= ' act';%' ppi';
+        type= ' ppi';%' act';
         plot_sharedreward(name, roidir, rois, models, R_Friend, R_Stranger, R_Computer, P_Friend, P_Stranger, P_Computer, type)  
 end
